369d47afbd8e1baaaea0b0b755f859396d7aa04e
Ukuer <2032108783@qq.com>
2019-10-13 23:06:36 +0800

    10.13

---
+++ mytermux/homework/Stack.h
@@ -0,0 +1,61 @@
+/*
+ * Stack.h
+ *
+ *  Created on: 2019年10月13日
+ *      Author: Jianlong  Jin
+ */
+#include <iostream>
+using namespace std;
+typedef int ElementType;
+
+#ifndef STACK_H_
+#define STACK_H_
+#define MAXSIZE 100
+class AStack {
+    ElementType *Data;
+    int top;
+    int size;
+public:
+    AStack(int n);
+    ~AStack();
+    void Push(ElementType item);
+    ElementType Pop();
+    bool Empty();
+    bool Full();
+};
+
+#endif /* STACK_H_ */
+
+AStack::AStack(int n){
+    Data = new ElementType[MAXSIZE];
+    top = -1;
+    size = n;
+}
+
+AStack::~AStack(){
+    delete Data;
+}
+
+void AStack::Push(ElementType item){
+    if (top == MAXSIZE-1){
+        cout << "Stack is full" << endl;
+        return ;
+    }
+    Data[++ top] = item;
+}
+
+ElementType AStack::Pop(){
+    return Data[top --];
+}
+
+bool AStack::Empty(){
+    if (top == -1)
+        return true;
+    return false;
+}
+
+bool AStack::Full(){
+	if (top == size-1)
+		return true;
+	return false;
+}
---
+++ mytermux/homework/text.cpp
@@ -0,0 +1,59 @@
+/*
+ * text.cpp
+ *
+ *  Created on: 2019年10月13日
+ *      Author: Jianlong  Jin
+ */
+#include "Stack.h"
+typedef AStack Stack;
+bool mypush(Stack S, int a[], int &index, int n){	// 实现自动判断能否压入
+	while (n--){
+		if (S.Full() || index >= 4)
+			return false;
+		S.Push(a[index]);
+		++index;
+	}
+	return true;
+}
+
+bool mypop(Stack S, int a[], int n){
+	while (n--){
+		if (S.Empty())
+			return false;
+		cout << S.Pop();
+	}
+	return true;
+}
+
+void push_all(Stack S, int a[], int index);
+void pop_all(Stack S, int a[], int index);
+
+void push_all(Stack S, int a[], int index){
+	for (int i=1; i<=4; ++i){
+		cout << endl;
+		if (index>=4)	break;
+		int this_index = index;
+		if (!mypush(S, a, this_index, i))
+			break;
+		pop_all(S, a, this_index);
+	}
+	return;
+}
+
+void pop_all(Stack S, int a[], int index){
+	for (int i=1; i<=4; ++i){
+		if (!mypop(S, a, i))
+			break;
+		push_all(S, a, index);
+	}
+	return ;
+}
+
+int main(){
+	Stack S(4);
+	int a[4]{1,2,3,4};
+	int index = 0;
+	push_all(S, a, index);
+
+	return 0;
+}
