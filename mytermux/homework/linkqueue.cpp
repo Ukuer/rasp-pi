948cc36497452a4451c195c8549ddfa56797bacd
Ukuer <2032108783@qq.com>
2019-10-16 13:31:31 +0800

    10.16

--- mytermux/homework/linkqueue.cpp
+++ mytermux/homework/linkqueue.cpp
@@ -8,13 +8,35 @@ public:
 		void push(ElementType item);
 		ElementType pop();
 		bool empty();
-		bool full;
-		bool sz;
+		bool full();
+		bool size();
 };
 void LinkQueue::push(ElementType item){
 		tail->data = item;
 		tail = tail->next;
 		++sz;
 }
-ElementType LinkQueue::pop
+ElementType LinkQueue::pop(){
+	if (this->empty()){
+		cout << "queue is empty" << endl;
+		return ;
+	}
+	ElementType res = head->data;
+	head = head->next;
+	--sz;
+	return res;
+}
+bool LinkQueue::empty(){
+	if (head==tail)
+		return true;
+	return false;
+}
+bool LinkQueue::full(){
+	if (tail->next == head)
+		return true;
+	return false;
+}
+int LinkQueue::size(){
+	return sz;
+}
 
