0a39ceae5fb8be92f76d1d3e6abc90c5d5122d36
Ukuer <2032108783@qq.com>
2019-11-04 10:45:32 +0800

    add 15

--- mytermux/homework/C5/0515.c
+++ mytermux/homework/C5/0515.c
@@ -9,4 +9,55 @@
 int main(){
 	
 	int m[5][5]{(17,24,1,8,15),
-		(
+		(23,5,7,14,16),
+		(4,6,13,20,22),
+		(10,12,19,21,3),
+		(11,18,25,2,9)};
+
+	int sum=0, i=0, j=0;
+	for (i=0, i<5; ++i)
+		sum = m[0][i];
+
+	// check every row
+	for (i=0; i<5; ++i){
+		int s = 0;
+		for (j=0; j<5; ++j)
+			s += m[i][j];
+		if (s != sum){
+			printf("No\n");
+			return 0;
+		}
+	}
+
+	// check every vol
+	for (i=0; i<5; ++i){
+		int s = 0;
+		for (j=0; j<5; ++j)
+			s += m[j][i];
+		if (s != sum){
+			printf("No\n");
+			return 0;
+		}
+	}
+
+	// check angleline
+	int s=0;
+	for (i=0, i<5; ++i)
+		s += m[i][i];
+	if (s != sum){
+		printf("No\n");
+		return 0;
+	}
+	
+	// check reangleline
+	s = 0;
+	for (i=0; i<5; ++i)
+		s += m[i][4-1];
+	if (s != sum)
+		printf("No\n");
+		return 0;
+	}
+
+	printf("Yes\n");
+	return 0;
+}
